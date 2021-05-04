c22675ffe6590ea111661ab7e61636e03f97c8f1
Israel7KerneL <israelaguilarcruz@gmail.com>
2021-05-04 10:27:23 -0500

    Pendiente generar condición verifica la extensión del archivo para uso de sustitución de espacios.

--- DownloadDirectJS.php
+++ DownloadDirectJS.php
@@ -9,8 +9,14 @@
  {
      /* TEST NOMBDE DE ARCHIVO CON ESPACIÓN Y REPLAZO DE ARCHIVOS POR CODE % USADO POR INTERNET */
     var filename = 'Manual 20FC 20Franquiciatario 2021.pdf';
+    // var filename = 'Nombre Sepa ZIP.zip';
     var replaced = filename.split(' ').join('%');
     alert(replaced);
+
+    /* 
+        PROGRAMAR CONDICIONAL PARA VERIFICAR EXTENSIÓN, SI ES PDF SE SUSTITUYEN LOS ESPACIOS
+        DE LO CONTRARIO NO ES NECESARIO SUSTITUR ESPACIOS DENTRO DE NOMBRE
+     */
 
     // window.open("http://franquiciascostanzo.com/PlataformaFranquicias/Franquicias_Manuales/Manual%20FC%20Franquiciatario%2021.pdf", 'Download');
     window.open("http://franquiciascostanzo.com/PlataformaFranquicias/Franquicias_Manuales/"+replaced);
